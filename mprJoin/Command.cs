ae975efb8920118d833f47fa339624b86ebf588d
Александр Пекшев <cjmodis@gmail.com>
2021-11-09 21:45:22 +0300

    Add skeleton

--- mprJoin/Command.cs
+++ mprJoin/Command.cs
@@ -15,6 +15,13 @@
         {
             try
             {
+                var win = new MainWindow
+                {
+                    DataContext = new MainContext()
+                };
+
+                ModPlus_Revit.ModPlus.ShowModal(win);
+
                 return Result.Succeeded;
             }
             catch (Autodesk.Revit.Exceptions.OperationCanceledException)
---
+++ mprJoin/MainContext.cs
@@ -0,0 +1,11 @@
+﻿namespace mprJoin
+{
+    using ModPlusAPI.Mvvm;
+
+    /// <summary>
+    /// Main context
+    /// </summary>
+    internal class MainContext : ObservableObject
+    {
+    }
+}

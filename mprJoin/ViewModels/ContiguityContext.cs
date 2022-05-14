4b8c1eba6bc0f119ac211c14e0cf04541e080c27
AlexNik235 <undead235@gmail.com>
2022-05-14 13:31:07 +0300

    add apply filter for contuguityControl

--- mprJoin/ViewModels/ContiguityContext.cs
+++ mprJoin/ViewModels/ContiguityContext.cs
@@ -9,6 +9,8 @@ using Autodesk.Revit.UI;
 using Enums;
 using Models;
 using ModPlus_Revit;
+using ModPlus_Revit.Enums;
+using ModPlus_Revit.Utils;
 using ModPlusAPI;
 using ModPlusAPI.Mvvm;
 using ModPlusAPI.Services;
@@ -30,8 +32,12 @@ public class ContiguityContext : BaseContext
         _collectorService = new CollectorService();
         _uiApplication = uiApplication;
         _elementConnectorService = new ElementConnectorService(uiApplication.ActiveUIDocument);
-        var list = UserSettingsService.Get<ObservableCollection<SelectedCategory>>(nameof(SelectedCategories));
-        _selectedCategories = list.Any() ? list : null;
+        Filter = UserSettingsService
+           .Get<ElementApplyFilter>(nameof(Filter));
+        if (Filter.SourceCategoriesOverride == null)
+        {
+            Filter.SourceCategoriesOverride = PluginSetting.AllowedCategoriesToContiguity;
+        }
 
         ContiguityOption = UserSettingsService.Get<ContiguityOption>(nameof(ContiguityOption));
         FirstEnd = UserSettingsService.Get<bool>(nameof(FirstEnd));
@@ -44,10 +50,9 @@ public class ContiguityContext : BaseContext
     public ContiguityOption ContiguityOption { get; set; }
 
     /// <summary>
-    /// Список моделей категорий для вывода пользователю
+    /// Фильтр
     /// </summary>
-    public ObservableCollection<SelectedCategory> SelectedCategories =>
-        _selectedCategories ??= new ObservableCollection<SelectedCategory>(GetSelectedCategories(PluginSetting.AllowedCategoriesToContiguity));
+    public ElementApplyFilter Filter { get; set; }
 
     /// <summary>
     /// Обрабатывать ли начало элемента
@@ -71,7 +76,6 @@ public class ContiguityContext : BaseContext
                 if (scope == ScopeType.SelectedElement)
                     MainWindow.Hide();
 
-                var selectedCategories = SelectedCategories.Where(i => i.IsSelected).Select(i => i.Name).ToList();
                 var elements = _collectorService
                     .GetFilteredElementCollector(_uiApplication.ActiveUIDocument, scope)
                     .WhereElementIsNotElementType()
@@ -81,7 +85,7 @@ public class ContiguityContext : BaseContext
                     .WherePasses(new ElementMulticategoryFilter(PluginSetting.AllowedCategoriesToContiguity))
 
                     // Оставляет только выбранные
-                    .Where(element => selectedCategories.Contains(element.Category.Name))
+                    .Where(element => Filter.IsMatch(element))
                     .ToList();
                 if (!elements.Any())
                 {
@@ -116,7 +120,7 @@ public class ContiguityContext : BaseContext
     /// </summary>
     public override void SaveSettings()
     {
-        UserSettingsService.Set(SelectedCategories, nameof(SelectedCategories));
+        UserSettingsService.Set(Filter, nameof(Filter));
         UserSettingsService.Set(ContiguityOption, nameof(ContiguityOption));
         UserSettingsService.Set(FirstEnd, nameof(FirstEnd));
         UserSettingsService.Set(SecondEnd, nameof(SecondEnd));
